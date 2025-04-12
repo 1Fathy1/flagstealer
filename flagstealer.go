f26ebfc9a144b7874048c73d6ac2428d359594b2
Fathy Moamen <163406667+1Fathy1@users.noreply.github.com>
2025-04-12 09:30:24 +0200

    Update flagstealer.go

--- flagstealer.go
+++ flagstealer.go
@@ -1,18 +1,8 @@
 package flagstealer
 
 import "fmt"
-import "os"
 
 func main() {
     fmt.Println("hi fathy")
-    f, err := os.Open("/root/flag.txt")
-    if err != nil {
-        fmt.Println("can't open flag: " + err.Error())
-    }
-    defer f.Close()
-
-    data := make([]byte, 1024)
-    n, _ := f.Read(data)        // قراءة البيانات
-    fmt.Println("FLAG: " + string(data[:n])) // طباعة الفلاج
 }
 
